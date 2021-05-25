9e688929ffe86e2be3294e11b1fb620cd034aba6
yuan <987607481@qq.com>
2021-05-25 15:39:08 +0800

    GitHub Pages测试

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,7 @@ export default defineConfig({
       '@': resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/mgblog' : './', // 设置打包路径
-
+  base: './', // 设置打包路径
   server: {
     port: 4000, // 设置服务启动端口号
     open: true, // 设置服务启动时是否自动打开浏览器
